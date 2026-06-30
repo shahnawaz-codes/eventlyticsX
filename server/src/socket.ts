be706f3ac673633ad2ad5f6c24f4988090ef0dbf
shahnawaz khan <khan.dev006@gmail.com>
2026-06-30 16:45:09 +0530

    feat(socket): implement project-specific room joining
    Replace generic message handlers with a `join-project` event that allows clients to join specific project rooms using a project key. This enables targeted real-time updates for individual project dashboards.

--- server/src/socket.ts
+++ server/src/socket.ts
@@ -3,22 +3,27 @@ import { Server } from "socket.io";
 import http from "http";
 
 const app = expres();
-
+// create http server
 const server = http.createServer(app);
+// accept the socket upgrade req and set the cors config for security
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
+// set io obj so that any controller can access it easily using req.app.get("io")
 app.set("io", io);
+
+//---------------------------------------------------------
+// establish the connection b/w server and client
 io.on("connection", (socket) => {
+
   console.log("hello bro", socket.id);
-  socket.on("msg", (data) => {
-    console.log("client:", data?.msg);
-  });
-  socket.on("msg2", (data) => {
-    console.log("client:", data?.msg);
+  // join perticular project and receive the projectId from client
+  socket.on("join-project", ({ projectKey }) => {
+    console.log("client:", projectKey);
+    socket.join(`dashboard:${projectKey}`);
   });
   socket.on("disconnect", () => {
     console.log("❌ Socket disconnected:", socket.id);
